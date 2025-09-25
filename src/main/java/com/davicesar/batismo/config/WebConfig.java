a31309cce3a20dbe899090a9ec6ddc23581f0f55
davicesarm <davicesarmorais@gmail.com>
2025-09-25 02:54:24 -0300

    fix: cors

--- src/main/java/com/davicesar/batismo/config/WebConfig.java
+++ src/main/java/com/davicesar/batismo/config/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
+        registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedMethods("GET", "POST", "OPTIONS", "PATCH")
                 .allowCredentials(true);
     }
 }
